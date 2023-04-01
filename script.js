d8138bec9e338fa261fbcd7b17f4571fcba83b0a
Arsonela <87339151+arso-k@users.noreply.github.com>
2023-04-01 22:58:52 +0200

    change of colors, addin another animation

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const boxes = document.querySelectorAll(".box");
+// fireout an event
 window.addEventListener("scroll", checkBoxes);
-
+// first execution
 checkBoxes();
+//
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
 
+  //  we will loop through each box
   boxes.forEach((box) => {
+    // providesinformation about the size of an element and its position relative to the viewport. and we need to know where the top is
     const boxTop = box.getBoundingClientRect().top;
 
     if (boxTop < triggerBottom) {
+      // we will add the box
       box.classList.add("show");
     } else {
+      // we will remove
       box.classList.remove("show");
     }
   });
